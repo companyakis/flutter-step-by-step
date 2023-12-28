8c9f9e20b63eec017e6023c16922403d4369d8d2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-28 20:38:41 +0300

    Create A-2-2-Column.dart

---
+++ A-2-2-Column.dart
@@ -0,0 +1,26 @@
+//mainpage.dart file
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Flutter Step by Step")),
+        body: Column(
+          children: [
+            Container(width: 100, height: 95, color: Colors.purple),
+            Container(width: 190, height: 75, color: Colors.green),
+            Container(width: 60, height: 70, color: Colors.blueAccent),
+            const Text("Three shapes"),
+          ],
+        ));
+  }
+}
