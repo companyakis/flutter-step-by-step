77e8f8729b807a088ebcc45ef3730394d0479e17
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-28 20:18:46 +0300

    Create A-1-HelloFlutter.dart

---
+++ A-1-HelloFlutter.dart
@@ -0,0 +1,50 @@
+//mainpage.dart file
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter Step by Step")),
+      body: Column(children: [
+        const Text("Hello Flutter"),
+      ]),
+    );
+  }
+}
+
+//------------------------------------------------------
+
+//main.dart file
+
+import 'package:flutter/material.dart';
+import 'package:fzt/mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainPage(),
+    );
+  }
+}
