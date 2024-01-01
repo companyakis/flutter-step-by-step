9712bf4d6a3d0da46c89386ed786818bf86f1308
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-01 18:49:49 +0300

    Create A-3-6-CustomWidgets2.dart

---
+++ A-3-6-CustomWidgets2.dart
@@ -0,0 +1,63 @@
+//mainpage.dart file
+
+import 'package:flutter/material.dart';
+import 'package:fzt/custom_widgets.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter Step by Step")),
+      body: const Center(
+        child: Column(
+          children: [
+            BlueRectangular(),
+            SizedBox(height: 15),
+            BlueRectangular(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//-----------------------------------------!!!!!!!!!-------------------------------
+
+//main.dart file
+
+//main.dart file
+
+import 'package:flutter/material.dart';
+import 'package:fzt/mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+
+
+
