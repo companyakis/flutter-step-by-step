823b8225ada3f23c677c133183196e5c2995265c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-01 19:10:23 +0300

    Update A-3-6-CustomWidgets5.dart

--- A-3-6-CustomWidgets5.dart
+++ A-3-6-CustomWidgets5.dart
@@ -1,17 +1,29 @@
-//custom_widgets.dart file
+//mainpage.dart file
 
 import 'package:flutter/material.dart';
+import 'package:fzt/custom_widgets.dart';
 
-//greeting custom widget
-
-class Greeting extends StatelessWidget {
-  String ourGreeting;
-  double ourFontSize;
-
-  Greeting({required this.ourGreeting, required this.ourFontSize});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
-    return Text(ourGreeting, style: TextStyle(fontSize: ourFontSize));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter Step by Step")),
+      body: Center(
+        child: Column(
+          children: [
+            Greeting(ourGreeting: "Hi there", ourFontSize: 40.0),
+            const SizedBox(height: 35),
+            Greeting(ourGreeting: "How are you?", ourFontSize: 20.0)
+          ],
+        ),
+      ),
+    );
   }
 }
