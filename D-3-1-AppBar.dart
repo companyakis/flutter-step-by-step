0f376b268aeab4d12fe3df1aeb2a90c84d19682e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-30 19:11:49 +0300

    Create D-3-1-AppBar.dart

---
+++ D-3-1-AppBar.dart
@@ -0,0 +1,45 @@
+//main_page.dart file
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("DeFi App",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+            Text("Just Use",
+                style: TextStyle(color: Colors.white, fontSize: 15)),
+          ],
+        ),
+        centerTitle: false,
+        leading: IconButton(
+            tooltip: "Services",
+            onPressed: () {},
+            icon: const Icon(Icons.monetization_on_outlined)),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text("Need Help?",
+                  style: TextStyle(color: Colors.white))),
+        ],
+      ),
+      body: const Center(),
+    );
+  }
+}
